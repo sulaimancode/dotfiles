1df8ba658e31f3fe9f324d546ea0f4c81a7b11b1
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-02-24 21:25:42 +0000

    add svelte to list of filetypes for prettierd

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -14,11 +14,10 @@ end
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettierd.with({ extra_filetypes = { "svelte" } }),
     null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
+        diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
-    null_ls.builtins.diagnostics.fish
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
