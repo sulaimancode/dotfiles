40e60e90107bc966ed9fbacc3ada8e67f6299dc3
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-02-24 22:55:29 +0000

    set powershell as nvim shell

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -115,6 +115,15 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
+-- Set the default shell to powershell - windows only 
+vim.cmd [[
+		let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+		let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		set shellquote= shellxquote=
+]]
+
 
 -- Fix tabs 
 vim.opt.tabstop = 4
