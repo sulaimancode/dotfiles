be714ed1901134fdda65dc5566a869603127f339
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-02-24 22:56:10 +0000

    Update vscode-colorscheme.lua

--- .config/nvim/after/plugin/vscode-colorscheme.lua
+++ .config/nvim/after/plugin/vscode-colorscheme.lua
@@ -7,7 +7,7 @@ vim.o.background = 'dark'
 local c = require('vscode.colors').get_colors()
 require('vscode').setup({
     -- Enable transparent background
-    transparent = true,
+    -- transparent = true,
 
     -- Enable italic comment
     italic_comments = true,
