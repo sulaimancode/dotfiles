eaa463349d8442baae7a894300aa19afe781d0b8
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-02-25 00:32:28 +0000

    add lua formatting and format lua files
    revert setting shell to powershell - apparently this messes with some autocmds as they behave differently in powershell

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -4,5 +4,10 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<Tab>", function() ui.nav_next() end)
-vim.keymap.set("n", "<S-Tab>", function() ui.nav_prev() end)
+vim.keymap.set("n", "<leader><Tab>", function()
+	ui.nav_next()
+end)
+
+vim.keymap.set("n", "<leader><S-Tab>", function()
+	ui.nav_prev()
+end)
